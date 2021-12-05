0285734965b50472409bbc14690cecf9b05a4169
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2021-12-05 21:37:30 +0530

    Pattern in java

---
+++ lectures-003/Patt.java
@@ -0,0 +1,11 @@
+public class Patt {
+    public static void main(String[] args) {
+
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+    System.out.println("******");
+    }
+}
--- lectures-003/Pattern.java
+++ lectures-003/Pattern.java
@@ -4,7 +4,6 @@ public class Pattern {
      {
          System.out.println("*");
      }
-
-       
+   
     }
 
