6693a5ed8c7670059015f5956d955519ef8069ad
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2021-12-05 19:58:11 +0530

    Input Sun

---
+++ lectures-002/inputSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class inputSum {
+
+    public static void main(String[] args) {
+      
+    Scanner input = new Scanner(System.in);
+    int num1 = input.nextInt();
+    int num2 = input.nextInt();
+
+    int sum = num1+num2;
+
+    System.out.println("Sum =" +sum);
+
+
+}
+    
+}
